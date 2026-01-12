fb7c90072068183f3b4553197a825dff647e34bb
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-13 00:06:55 +0530

    break and continue

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,39 @@
 //     }
 // }
 
-fn main(){
-    let mut count =0;
-    println!("lets count untill infinity");
-    //infinity loop
-    loop{
-        count += 1;
-        if count == 3{
-            println!("three");
-            continue;
-        }
-        println!("{}", count);
-        if count == 5{
-            println!("ok thats enough");
-            break;
-        }
+// fn main(){
+//     let mut count =0;
+//     println!("lets count untill infinity");
+//     //infinity loop
+//     loop{
+//         count += 1;
+//         if count == 3{
+//             println!("three");
+//             continue;
+//         }
+//         println!("{}", count);
+//         if count == 5{
+//             println!("ok thats enough");
+//             break;
+//         }
 
 
         
+//     }
+// }
+
+
+
+fn main(){
+    'outer:loop{
+        println!("entered the outer loop");
+        
+        'inner:loop{
+            println!("entered the inner loop");
+
+            break 'outer;
+        }
+        println!("this point will never be reached");
     }
+    println!("exited from the outer loop");
 }
