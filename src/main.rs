0a56511a1201106ad3ad0f1ec69d5e983ac15628
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-12 23:39:12 +0530

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-fn main() {
+fn main(){
     println!("Hello, world!");
-    let x = 5;
-    let y: u32 = 10;
-    let z: f32= 3.14;
-    println!("x:{}",x);
+    for i in 0..10{
+        println!("Number: {}", i);
+    }
 }
