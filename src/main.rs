80c321f5084b16bebe0362d446eb49a2486807a6
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-12 23:59:26 +0530

    infinity loops

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+// fn main(){
+//     println!("Hello, world!");
+//     for i in 0..10{
+//         println!("Number: {}", i);
+//     }
+// }
+
 fn main(){
-    println!("Hello, world!");
-    for i in 0..10{
-        println!("Number: {}", i);
+    let mut count =0;
+    println!("lets count untill infinity");
+    //infinity loop
+    loop{
+        count += 1;
+        if count == 3{
+            println!("three");
+            continue;
+        }
+        println!("{}", count);
+        if count == 5{
+            println!("ok thats enough");
+            break;
+        }
+
+
+        
     }
 }
