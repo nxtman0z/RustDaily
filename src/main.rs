dda5755e13a2ba5615e11e9992ea7e7804f85890
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-14 22:05:03 +0530

    while loop code

--- src/main.rs
+++ src/main.rs
@@ -27,17 +27,24 @@
 // }
 
 
-fn main(){
-    let mut counter = 0;
-    let result = loop{
-        counter += 1;
+// fn main(){
+//     let mut counter = 0;
+//     let result = loop{
+//         counter += 1;
 
-        if counter == 10{
-            break counter *2;
-        }
-    };
-    assert_eq!(result, 20);
+//         if counter == 10{
+//             break counter *2;
+//         }
+//     };
+//     assert_eq!(result, 20);
+// }
+
+
+
+fn main() {
+    let mut i = 0;
+    while i < 10 {
+        println!("hello");
+        i += 1;
+    }
 }
-
-
-
